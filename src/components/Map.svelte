bfc2d3e88f167ea36bbea9e1427ae97aea73e0b7
Robin Monseré <51537026+robinmonsere@users.noreply.github.com>
2024-02-22 22:26:37 +0100

    Adding .env.example

---
+++ src/components/Map.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { onMount, onDestroy } from 'svelte'
+    import {Map, MapStyle, config} from '@maptiler/sdk';
+    import "@maptiler/sdk/dist/maptiler-sdk.css";
+
+    let map;
+    let mapContainer;
+
+    config.apiKey = import.meta.env.VITE_PUBLIC_MAP_KEY;
+
+    onMount(() => {
+        const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+
+        map = new Map({
+            container: mapContainer,
+            style: MapStyle.STREETS,
+            center: [initialState.lng, initialState.lat],
+            zoom: initialState.zoom
+        });
+
+    });
+
+</script>
+
+<div class="map-wrap">
+    <div class="map" bind:this={mapContainer}></div>
+</div>
+
+<style>
+    .map-wrap {
+        position: relative;
+        width: 100%;
+        height: calc(100vh - 77px); /* calculate height of the screen minus the heading */
+    }
+
+    .map {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+    import Map from '../components/Map.svelte';
+</script>
+
+<main>
+    <Map />
+</main>
